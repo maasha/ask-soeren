faca18b1cb5e24bcefd30933affaa30c487980af
Martin Asser Hansen <mail@maasha.dk>
2024-01-08 18:14:09 +0100

    Adding prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-airbnb-with-typescript',
+    'plugin:prettier/recommended',
+    // "plugin:vue/essential",
+    // "eslint:recommended"
+  ],
+}
