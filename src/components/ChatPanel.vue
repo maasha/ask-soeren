c5f400492dd200651d48690152d305f86d084d39
Martin Asser Hansen <mail@maasha.dk>
2024-01-08 21:16:41 +0100

    Adding form to chat panel

--- src/components/ChatPanel.vue
+++ src/components/ChatPanel.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>ChatPanel</h1>
+  <div class="border w-full p-4">
+    <form @submit.prevent="">
+      <label for="textInput" class="text-3xl">Post your question</label>
+      <input
+        class="border w-full mt-5 text-3xl p-3"
+        type="text"
+        id="textInput"
+        placeholder="here..."
+      />
+      <footer class="flex justify-center mt-5">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 active:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Ask Søren
+        </button>
+      </footer>
+    </form>
+  </div>
 </template>
