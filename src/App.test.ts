5aed14bd64fc84044ad75b4a12485bcb52668766
Martin Asser Hansen <mail@maasha.dk>
2024-01-10 11:49:45 +0100

    Rename AppVue to App

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,37 +1,37 @@
 import { render, screen } from '@testing-library/vue'
-import AppVue from './App.vue'
+import App from './App.vue'
 
 describe('App', () => {
   it('should have correct title', () => {
-    render(AppVue)
+    render(App)
 
     const element = screen.getByTestId('title')
     expect(element.textContent).toBe('Ask Søren')
   })
 
   it('should have correct subtitle', () => {
-    render(AppVue)
+    render(App)
 
     const element = screen.getByTestId('subtitle')
     expect(element.textContent).toBe('(... about Pharma & IT)')
   })
 
   it('should contain a QuestionPanel component', () => {
-    render(AppVue)
+    render(App)
 
     const element = screen.getByTestId('question-panel')
     expect(element).toBeTruthy()
   })
 
   it('should contain an AnswerPanel component', () => {
-    render(AppVue)
+    render(App)
 
     const element = screen.getByTestId('answer-panel')
     expect(element).toBeTruthy()
   })
 
   it('should contain an AvatarPanel component', () => {
-    render(AppVue)
+    render(App)
 
     const element = screen.getByTestId('avatar-panel')
     expect(element).toBeTruthy()
