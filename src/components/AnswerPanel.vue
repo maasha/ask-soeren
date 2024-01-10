25f7719a2dc5b311cd02ce8780666db6f7eca1b7
Martin Asser Hansen <mail@maasha.dk>
2024-01-10 14:52:40 +0100

    Converting to use Composition API

--- src/components/AnswerPanel.vue
+++ src/components/AnswerPanel.vue
@@ -1,28 +1,20 @@
+<script setup lang="ts">
+import { PropType, defineProps } from 'vue'
+
+const props = defineProps({
+  answerText: {
+    type: String as PropType<string | null>,
+    default: null,
+  },
+})
+</script>
+
 <template>
   <div
     data-testid="answer-panel"
     class="border w-1/3 p-4 max-h-96 overflow-y-auto flex flex-col-reverse"
   >
-    <p v-if="answerText" class="text-xl mt-5">{{ answerText }}</p>
+    <p v-if="answerText" class="text-xl mt-5">{{ props.answerText }}</p>
     <h3 v-if="answerText" class="text-3xl mt-5">Søren says:</h3>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
-
-/**
- * Panel to display the answer from Søren.
- */
-export default defineComponent({
-  props: {
-    /**
-     * The answer text to display.
-     */
-    answerText: {
-      type: String as PropType<string | null>,
-      default: null,
-    },
-  },
-})
-</script>
