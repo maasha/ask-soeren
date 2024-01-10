b7fcd2a9f00e94d645c7bac160bd87d1cfd2d7da
Martin Asser Hansen <mail@maasha.dk>
2024-01-10 11:41:08 +0100

    AnswerPanel unit tests ok

--- src/components/AnswerPanel.test.ts
+++ src/components/AnswerPanel.test.ts
@@ -1,10 +1,42 @@
+// Import necessary functions from testing library
 import { render, screen } from '@testing-library/vue'
-
 import AnswerPanel from './AnswerPanel.vue'
 
-describe('AnswerPanel', async () => {
-  it('should render', () => {
-    render(AnswerPanel)
-    expect(screen.getByText('Søren says:'))
+describe('AnswerPanel', () => {
+  it('renders "Søren says" if answerText is provided', async () => {
+    const answerText = 'This is the answer text.'
+
+    render(AnswerPanel, {
+      props: {
+        answerText,
+      },
+    })
+
+    expect(screen.findByText(answerText)).resolves.toBeDefined()
+  })
+
+  it('renders answer text if answerText is provided', async () => {
+    const answerText = 'This is the answer text.'
+
+    render(AnswerPanel, {
+      props: {
+        answerText,
+      },
+    })
+
+    expect(screen.findByText('Søren says:')).resolves.toBeDefined()
+  })
+
+  it('does not render answer text when answerText is null', async () => {
+    // Render the component with null answerText
+    render(AnswerPanel, {
+      props: {
+        answerText: null,
+      },
+    })
+
+    const element = screen.getByTestId('answer-panel')
+
+    expect(element.textContent).toBe('')
   })
 })
