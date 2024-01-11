5783b648e91be4eacdf8134e2e76a413b4e0dfa9
Martin Asser Hansen <mail@maasha.dk>
2024-01-11 22:20:39 +0100

    Cleanup of e2e test

--- cypress/e2e/AskSoeren.cy.ts
+++ cypress/e2e/AskSoeren.cy.ts
@@ -1,69 +1,43 @@
 /// <reference types="cypress" />
 
 describe('AskSoeren', () => {
-  it('successfully loads', () => {
+  it('should load the page successfully', () => {
     cy.visit('/')
+    cy.get('[data-testid="title"]').should('include.text', 'Ask Søren')
   })
 
-  const response = {
-    choices: [
-      {
-        message: {
-          content: 'foo',
-        },
-      },
-    ],
-  }
+  it('should be possible to select an answer style', () => {
+    cy.visit('/')
+    cy.get('[data-testid="style-select"]').select('British High Class')
+    cy.get('[data-testid="style-select"]').should('have.value', 'british')
+  })
 
-  it('spy', () => {
+  it('should be possible to post a question and obtain an answer', () => {
+    const response = {
+      choices: [
+        {
+          message: {
+            content: 'foo',
+          },
+        },
+      ],
+    }
+
     cy.intercept('POST', 'https://api.openai.com/v1/chat/completions', {
       statusCode: 200,
       body: response,
     }).as('askOpenAI')
 
     cy.visit('/')
+    cy.get('[data-testid="answer-heading"]').should('not.exist')
+    cy.get('[data-testid="answer-text"]').should('not.exist')
     cy.get('[data-testid="question-input"]').type('what is IQ?')
     cy.get('[data-testid="submit-button"]').click()
 
     cy.wait('@askOpenAI').then((interception) => {
       expect(interception.response?.body).to.deep.equal(response)
     })
-  })
 
-  it.skip('should test something', () => {
-    cy.stubAskOpenAI().as('askOpenAI')
-    cy.visit('/')
-    // Your test code goes here
-    cy.get('[data-testid="question-input"]').type('what is IQ?')
-    cy.get('[data-testid="submit-button"]').click()
-    // You can now access the intercepted response using cy.wait('@askOpenAI')
-
-    try {
-      cy.wait('@askOpenAI').then((interception) => {
-        console.log({ interception })
-        console.log('Intercepted Response:', interception.response?.body)
-
-        const responseContent = interception.response?.body.choices[0].message.content
-      })
-    } catch (error) {
-      // Log or handle the error
-      cy.log(`Caught an error: ${error.message}`)
-    }
-    // Use responseContent as needed in your test
-  })
-
-  it.skip('should be possible to ask a question and get an answer', () => {
-    cy.visit('/')
-    cy.get('[data-testid="title"]').should('include.text', 'Ask Søren')
-
-    cy.get('[data-testid="answer-heading"]').should('not.exist')
-    cy.get('[data-testid="answer-text"]').should('not.exist')
-
-    cy.get('[data-testid="style-select"]').select('British High Class')
-    cy.get('[data-testid="style-select"]').should('have.value', 'british')
-
-    cy.get('[data-testid="question-input"]').type('what is IQ?')
-    cy.get('[data-testid="submit-button"]').click()
     cy.get('[data-testid="answer-heading"]').should('exist')
     cy.get('[data-testid="answer-text"]').should('exist')
   })
