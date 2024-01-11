de6b0b20763e2cd6dae0325024324fe7de959290
Martin Asser Hansen <mail@maasha.dk>
2024-01-11 18:01:19 +0100

    e2e test works, albeit slow

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,19 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(false)
+describe('AskSoeren', () => {
+  const url = 'http://localhost:5173/'
+
+  it('should be possible to ask a question and get an answer', () => {
+    cy.visit(url)
+    cy.get('[data-testid="title"]').should('include.text', 'Ask Søren')
+
+    cy.get('[data-testid="answer-heading"]').should('not.exist')
+    cy.get('[data-testid="answer-text"]').should('not.exist')
+
+    cy.get('[data-testid="style-select"]').select('British High Class')
+    cy.get('[data-testid="style-select"]').should('have.value', 'british')
+
+    cy.get('[data-testid="question-input"]').type('what is IQ?')
+    cy.get('[data-testid="submit-button"]').click()
+    cy.get('[data-testid="answer-heading"]').should('exist')
+    cy.get('[data-testid="answer-text"]').should('exist')
   })
 })
